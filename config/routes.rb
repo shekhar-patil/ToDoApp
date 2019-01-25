0f9318b366aed8aa9eeba4a2dfb49a5957fd8aca
shekhar-patil <patilshekhar900@gmail.com>
2019-01-25 13:45:44 +0530

    add controller Items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/new'
+  get 'items/show'
+  get 'items/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
