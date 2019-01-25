036816fcce7d474803319a54fcc7514473fe84fe
shekhar-patil <patilshekhar900@gmail.com>
2019-01-25 14:48:31 +0530

    add CRUD method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,18 +1,28 @@
 class Api::v1::ItemsController < ApplicationController
-  def new
-  end
-
-  def show
+  
+  def index
     render json: Item.all
   end
 
   def create
+    item = Item.create(item_params)
+    render json: item
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update_attributes(item_params)
+    render json: item
   end
 
   private
 
   def item_params
-    params.require(:item).parmit(:name , :mark)
+    params.require(:item).parmit(:id, :name , :mark)
   end
 
 end
